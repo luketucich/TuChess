10168e2e3f13f61cbf51e863ada02bb5329c2eb9
luketucich <ltucich@gmail.com>
2025-02-18 21:55:09 -0500

    style: Cleaned up the code in game.ts, color coded messages, and added whitespace to output for clarity.
    feat: Implemented player turn swaps after each move.

--- src/game.ts
+++ src/game.ts
@@ -1,28 +1,46 @@
 import { Board } from "./game/Board.ts";
 import * as readline from "readline";
 
+// Initialize the board and set the starting player
 const board = new Board();
 let currentPlayer = "White";
+const blackMessage = `\n\x1b[31m${"Black to move (start-end): \n"}\x1b[0m`;
+const whiteMessage = `\n\x1b[37m${"White to move (start-end): \n"}\x1b[0m`;
 
+// Create a readline interface for user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// Main game loop
 function gameLoop() {
-  console.log(board.displayBoard());
-  rl.question(`${currentPlayer} to move (start-end): `, (move) => {
-    const [start, end] = move.split("-");
-    try {
-      board.movePiece(start, end);
-    } catch {
-      console.log("Invalid move! Try again.");
+  // Display the current state of the board
+  board.displayBoard();
+
+  // Prompt the current player for their move
+  rl.question(
+    currentPlayer === "White" ? whiteMessage : blackMessage,
+    (move) => {
+      const [start, end] = move.split("-");
+      try {
+        // Attempt to move the piece
+        board.movePiece(start, end);
+      } catch {
+        // If the move is invalid, notify the player and restart the loop
+        console.log("Invalid move! Try again.");
+        gameLoop();
+        return;
+      }
+
+      // Swap the current player
+      currentPlayer = currentPlayer === "White" ? "Black" : "White";
+
+      // Continue the game loop
       gameLoop();
-      return;
     }
-    currentPlayer = currentPlayer === "White" ? "Black" : "White";
-    gameLoop();
-  });
+  );
 }
 
+// Start the game loop
 gameLoop();
